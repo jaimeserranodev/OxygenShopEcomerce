c3c3de644cfa760712893f11ac74fb43cb1b4799
jaimeserranodev <jaime.serrano.dev@gmail.com>
2023-04-10 14:55:24 +0100

    Agregado el modal

--- script.js
+++ script.js
@@ -39,35 +39,89 @@ document.addEventListener("DOMContentLoaded", function() {
     const nameInput = document.querySelector('#nameInput');
     const emailInput = document.querySelector('#emailInput');
     const checkbox = document.querySelector('#checkbox');
-  
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
-  
-      if (!checkbox.checked) {
-        alert('Please accept the legal disclaimer');
-        return;
-      }
-  
-      const formData = new FormData(form);
-  
-      fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-          name: nameInput.value,
-          email: emailInput.value,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error(error));
+    
+        form.addEventListener('submit', (event) => {
+        event.preventDefault();
+    
+        if (!checkbox.checked) {
+            alert('Please accept the legal disclaimer');
+            return;
+        }
+    
+        const formData = new FormData(form);
+    
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+            name: nameInput.value,
+            email: emailInput.value,
+            }),
+            headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+        });
     });
-  });
 
 
+// MODAL
+document.addEventListener("DOMContentLoaded", function() {
 
+    const modal = document.getElementById("modal");
+    const closeModal = document.getElementById("closeModal");
+    const form = document.getElementById("newsletterForm");
+    
+    // Mostrar modal después de 5 segundos
+    setTimeout(() => {
+        if (!localStorage.getItem("modalClosed")) {
+            showModal();
+        }
+    }, 5000);
+    
+    // Mostrar modal al bajar 25% de la página
+    window.addEventListener("scroll", () => {
+        if (!localStorage.getItem("modalClosed")) {
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+            const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight || 0;
+            const clientHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight || 0;
+    
+            if (scrollTop / (scrollHeight - clientHeight) >= 0.25) {
+                showModal();
+            }
+        }
+    });
+    
+    // Función para mostrar el modal
+    function showModal() {
+        modal.style.display = "block";
+    }
+    
+    // Función para cerrar el modal
+    function hideModal() {
+        modal.style.display = "none";
+        localStorage.setItem("modalClosed", "true");
+    }
+    
+    closeModal.addEventListener("click", hideModal);
+    
+    // Cerrar el modal al hacer clic fuera del contenido
+    modal.addEventListener("click", (event) => {
+        if (event.target === modal) {
+            hideModal();
+        }
+    });
+    
+    // Cerrar el modal con la tecla "Esc"
+    document.addEventListener("keydown", (event) => {
+        if (event.key === "Escape" || event.key === "Esc") {
+            hideModal();
+        }
+    });
+    
+    });
 
 
 
