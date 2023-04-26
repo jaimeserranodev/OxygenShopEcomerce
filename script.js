8ee84c240c349d2f85be563978f5cdef0d84e9ac
jaimeserranodev <jaime.serrano.dev@gmail.com>
2023-04-26 10:52:13 +0100

    agregado circulo de porcentaje y return to top

--- script.js
+++ script.js
@@ -14,37 +14,37 @@ function progresoScroll(){
 
 // window.addEventListener('scroll', progresoBarraHorizontal);
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     window.addEventListener("scroll", function() {
-//         const scrollProgress = document.getElementById("scroll-progress");
-//         const scrollPosition = window.scrollY;
-//         const totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-//         const percentage = (scrollPosition / totalHeight) * 100;
-//         scrollProgress.textContent = `${Math.round(percentage)}%`;
-//     });
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", function() {
+        const scrollProgress = document.getElementById("scroll-progress");
+        const scrollPosition = window.scrollY;
+        const totalHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const percentage = (scrollPosition / totalHeight) * 100;
+        scrollProgress.textContent = `${Math.round(percentage)}%`;
+    });
 
 
 
 
-// // BOTÓN RETURN TO TOP 
+// BOTÓN RETURN TO TOP 
 
-// const returnToTopButton = document.getElementById("return-to-top");
+const returnToTopButton = document.getElementById("return-to-top");
 
-// returnToTopButton.addEventListener("click", function () {
-//     window.scrollTo({
-//     top: 0,
-//     behavior: 'smooth',
-//     });
-// });
+returnToTopButton.addEventListener("click", function () {
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+    });
+});
 
-// window.addEventListener("scroll", function() {
-//     if (window.scrollY > 1000) {
-//     returnToTopButton.style.display = "block";
-//     } else {
-//     returnToTopButton.style.display = "none";
-//     }
-// });
-// });
+window.addEventListener("scroll", function() {
+    if (window.scrollY > 1000) {
+    returnToTopButton.style.display = "block";
+    } else {
+    returnToTopButton.style.display = "none";
+    }
+});
+});
 
 
 // // ENVIO DATOS FORMULARIO
